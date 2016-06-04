a839a075d9f68b584561741c247b72596df90db5
Jonathan Frederick <jrfreder@gmail.com>
2016-06-03 21:04:02 -0400

    testing for the existance of a flaskr_tdd database

--- app-test.py
+++ app-test.py
@@ -1,6 +1,7 @@
 from app import app
-
+import os
 import unittest
+import psycopg2
 
 
 class BasicTestCase(unittest.TestCase):
@@ -9,6 +10,13 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 404)
+
+    def test_database(self):
+        conn = psycopg2.connect(database='flaskr_tdd')
+        cursor = conn.cursor()
+        cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+        self.assertTrue(cursor.fetchall())
+
 
 
 if __name__ == '__main__':
