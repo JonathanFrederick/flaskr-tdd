b4f59bc0584d915bb1a5f62d8a4aa3e30066ba76
Jonathan Frederick <jrfreder@gmail.com>
2016-06-06 20:36:40 -0400

    sql command uses function parameter, some tidying

--- app.py
+++ app.py
@@ -14,24 +14,24 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
 db = SQLAlchemy(app)
 
-# function create a database
+
 def init_db(db_name=DATABASE):
+    """Function to create a database"""
     engine = create_engine('postgresql+psycopg2://127.0.0.1:5432/postgres')
     session = sessionmaker(bind=engine)()
     session.connection().connection.set_isolation_level(0)
-    session.execute('CREATE DATABASE {};'.format(DATABASE))
+    session.execute('CREATE DATABASE {};'.format(db_name))
     session.connection().connection.set_isolation_level(1)
 
-# function do drop a database
+
 def drop_db(db_name=DATABASE):
+    """Function to drop a database"""
     engine = create_engine('postgresql+psycopg2://127.0.0.1:5432/postgres')
     session = sessionmaker(bind=engine)()
     session.connection().connection.set_isolation_level(0)
-    session.execute('DROP DATABASE {};'.format(DATABASE))
+    session.execute('DROP DATABASE {};'.format(db_name))
     session.connection().connection.set_isolation_level(1)
-
 
 
 if __name__ == "__main__":
     app.run()
-#
