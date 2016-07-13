8d97d1edfa5ba880c9eb0bec2f1c76adb36fa02d
Jonathan Frederick <jrfreder@gmail.com>
2016-07-12 21:19:08 -0400

    testing for success at /

--- app-test.py
+++ app-test.py
@@ -11,7 +11,7 @@ class BasicTestCase(unittest.TestCase):
     def test_index(self):
         tester = app.app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_database(self):
         # conn = psycopg2.connect(database='flaskr_tdd')
